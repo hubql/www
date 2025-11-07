3f10bd696aec3b6a91879bbcb93f6c0cde23fcba
chriscodedesign <christophe.julien.leroy@gmail.com>
2025-11-07 11:52:15 +0700

    fix client

--- src/components/blocks/SplineScene.tsx
+++ src/components/blocks/SplineScene.tsx
@@ -1,7 +1,23 @@
 'use client'
 
-import Spline from '@splinetool/react-spline'
+import dynamic from 'next/dynamic'
+import { useState, useEffect } from 'react'
+
+const Spline = dynamic(
+    () => import('@splinetool/react-spline').then((mod) => mod.default),
+    { ssr: false }
+)
 
 export const SplineScene = ({ scene }: { scene: string }) => {
+    const [mounted, setMounted] = useState(false)
+
+    useEffect(() => {
+        setMounted(true)
+    }, [])
+
+    if (!mounted) {
+        return null
+    }
+
     return <Spline scene={scene} />
 }
